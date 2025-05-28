d7a48ec799c7ada9b7aac7afb8580d4e89f2382c
Hassan El Mghari <hassan4709@gmail.com>
2025-05-28 14:23:57 -0400

    wip

--- app/actions.ts
+++ app/actions.ts
@@ -4,7 +4,7 @@ import Together from 'together-ai';
 
 const together = new Together();
 
-export async function updateImage(imageUrl: string, prompt: string) {
+export async function generateImage(imageUrl: string, prompt: string) {
   const response = await together.images.create({
     // TODO: Update to the new model
     model: 'black-forest-labs/FLUX.1-depth',
--- app/page.tsx
+++ app/page.tsx
@@ -3,48 +3,83 @@
 
 import { useS3Upload } from 'next-s3-upload';
 import { useState } from 'react';
-import { updateImage } from './actions';
+import { generateImage } from './actions';
+import { useFormStatus } from 'react-dom';
 
 export default function Home() {
   const { uploadToS3 } = useS3Upload();
   const [imageUrls, setImageUrls] = useState<string[]>([]);
+  const [activeImageUrl, setActiveImageUrl] = useState<string | null>(null);
 
   const handleUpload = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
     if (file) {
       const result = await uploadToS3(file);
-      console.log(result);
-      setImageUrls([...imageUrls, result.url]);
+      setImageUrls([result.url]);
+      setActiveImageUrl(result.url);
     }
   };
 
   return (
-    <div className='flex flex-col items-center justify-center h-screen'>
-      {imageUrls.length === 0 ? (
-        <form className='flex flex-col items-center justify-center'>
-          <input type='file' accept='image/*' onChange={handleUpload} />
-        </form>
-      ) : (
-        <div className=''>
-          {imageUrls.map((url) => (
-            <img src={url} alt='uploaded image' key={url} />
-          ))}
-          <form
-            action={async (formData) => {
-              const prompt = formData.get('prompt') as string;
-              const imageUrl = imageUrls.at(-1);
-              if (!imageUrl) return;
-              const updatedImageUrl = await updateImage(imageUrl, prompt);
-              if (updatedImageUrl) {
-                setImageUrls([...imageUrls, updatedImageUrl]);
-              }
-            }}
-          >
-            <input type='text' name='prompt' placeholder='Make a change' />
-            <button type='submit'>Submit</button>
-          </form>
+    <div className='grid gap-2 grid-cols-6 max-w-2xl mx-auto pt-20'>
+      <div>
+        <div className='flex flex-col gap-2'>
+          {imageUrls
+            .slice()
+            .reverse()
+            .map((url) => (
+              <button key={url} onClick={() => setActiveImageUrl(url)}>
+                <img className='h-20' src={url} alt='uploaded image' />
+              </button>
+            ))}
         </div>
-      )}
+      </div>
+      <div className='flex col-span-5 flex-col items-center justify-center'>
+        {activeImageUrl === null ? (
+          <form className='flex flex-col items-center justify-center'>
+            <input type='file' accept='image/*' onChange={handleUpload} />
+          </form>
+        ) : (
+          <div className=''>
+            <div>
+              <img src={activeImageUrl} alt='uploaded image' className='' />
+            </div>
+
+            <form
+              action={async (formData) => {
+                const prompt = formData.get('prompt') as string;
+                const imageUrl = imageUrls.at(-1);
+                if (!imageUrl) return;
+                const generatedImage = await generateImage(imageUrl, prompt);
+                if (generatedImage) {
+                  setImageUrls((current) => [...current, generatedImage]);
+                  setActiveImageUrl(generatedImage);
+                }
+              }}
+            >
+              <input type='text' name='prompt' placeholder='Make a change' />
+              <SubmitButton />
+            </form>
+          </div>
+        )}
+      </div>
     </div>
+  );
+}
+
+function SubmitButton({
+  className,
+  ...rest
+}: React.ButtonHTMLAttributes<HTMLButtonElement>) {
+  const { pending } = useFormStatus();
+  return (
+    <button
+      className={`disabled:opacity-50 ${className}`}
+      type='submit'
+      disabled={pending}
+      {...rest}
+    >
+      Submit
+    </button>
   );
 }
